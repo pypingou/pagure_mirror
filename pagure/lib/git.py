07288042914ba8ec246f741adcc90e0f18d59cc4
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-11-15 15:24:53 +0100

    Support force push for project not on repospanner
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- pagure/lib/git.py
+++ pagure/lib/git.py
@@ -964,7 +964,7 @@ class TemporaryClone(object):
         """ Exit the context manager, removing the temorary clone. """
         shutil.rmtree(self.repopath)
 
-    def push(self, username, sbranch, tbranch=None, **extra):
+    def push(self, username, sbranch, tbranch=None, force=False, **extra):
         """ Push the repo back to its origin.
 
         Args:
@@ -1023,6 +1023,8 @@ class TemporaryClone(object):
             }
         else:
             command = ["git", "push", "origin"]
+            if force:
+                command.append("--force")
             environ = {}
 
         try:
